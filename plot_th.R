7ae5845c186d8b01c31e42d28ee17f428c5ffc92
Fernando Naufel <fnaufel@gmail.com>
2022-01-18 17:29:00 -0300

    First try at plot_th()

---
+++ plot_th.R
@@ -0,0 +1,91 @@
+
+plot_th <- function(
+  minimo = 0,
+  maximo = 1,
+  distr = dnorm,
+  distr_args = numeric(),
+  media = .5,
+  cauda,
+  erro,
+  tipo = 'ic'
+) {
+  
+  if (identical(distr, dt)) {
+    media_orig <- media
+    media <- 0
+  }
+  
+  p_cauda_esq <- media - abs(media - cauda)
+  p_cauda_dir <- media + abs(media - cauda)
+
+  if (identical(distr, dnorm))
+    args_to_use = c(mean = media, sd = erro)
+  
+  if (identical(distr, dt))
+    args_to_use = distr_args
+    
+  cauda_esq <- stat_function(
+    fun = distr,
+    args = args_to_use,
+    xlim = c(minimo, p_cauda_esq),
+    fill = 'red',
+    alpha = .5,
+    geom = 'area'
+  )
+
+  cauda_dir <- stat_function(
+    fun = distr,
+    args = args_to_use,
+    xlim = c(p_cauda_dir, maximo),
+    fill = 'red',
+    alpha = .5,
+    geom = 'area'
+  )
+  
+  meio <- stat_function(
+    fun = distr,
+    args = args_to_use,
+    xlim = c(p_cauda_esq, p_cauda_dir),
+    fill = 'red',
+    alpha = .5,
+    geom = 'area'
+  )
+
+  preencher <- switch (
+    tipo,
+    'ic'  = meio,
+    'bi'  = list(cauda_esq, cauda_dir),
+    'esq' = cauda_esq,
+    'dir' = cauda_dir
+  )
+  
+  brks <- c(minimo, p_cauda_esq, media, p_cauda_dir, maximo)
+  
+  if (tipo == 'esq')
+    brks <- brks[-4]
+
+  if (tipo == 'dir')
+    brks <- brks[-2]
+  
+  if (identical(distr, dt))
+    rotulos <- brks * erro + media
+  else
+    rotulos <- brks
+  
+  ggplot() +
+    stat_function(
+      fun = distr,
+      args = args_to_use,
+      xlim = c(minimo, maximo)
+    ) +
+    preencher +
+    scale_x_continuous(
+      breaks = brks,
+      labels = rotulos
+    ) +
+    labs(
+      y = NULL
+    ) +
+    theme(axis.text.x = element_text(angle = 90))
+  
+}
